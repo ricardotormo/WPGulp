754bff2c0d579825b25ead50846355c9d5ae60f4
Ricardo Tormo <ricardotormo.es@gmail.com>
2019-09-03 16:47:20 +0200

    promp interaction first try

--- installer/modules/run.js
+++ installer/modules/run.js
@@ -14,9 +14,29 @@ const handleError = require('./handleError.js');
 const clearConsole = require('./clearConsole.js');
 const printNextSteps = require('./printNextSteps.js');
 const prompts = require('prompts');
+const execDownload = (filesToDownload) => {
+	// Download.
+	return Promise.all(filesToDownload.map(x => download(x, `${theCWD}`))).then(async () => {
+		spinner.succeed();
+
+		// The npm install.
+		spinner.start('2. Installing npm packages...');
+		// await execa('npm', ['install', '--silent']);
+		await execa('npm', ['install']);
+		spinner.succeed();
+
+		// Done.
+		printNextSteps();
+	});
+}
 
 module.exports = () => {
 	(async () => {
+		const files = {
+			gulpFile: "https://raw.githubusercontent.com/ricardotormo/WPGulp/master/src/gulpfile.babel.js",
+			configFile: "https://raw.githubusercontent.com/ricardotormo/WPGulp/master/src/wpgulp.config.js",
+			packageJSONFile: "https://raw.githubusercontent.com/ricardotormo/WPGulp/master/src/package.json"
+		};
 		const questions = [
 			{
 				type: 'select',
@@ -30,44 +50,30 @@ module.exports = () => {
 			}
 		];
 		const onSubmit = (prompt, response) => {
-			console.log(`Thanks I got ${response} from ${prompt}`);
+			return response;
 		}
 		const response = await prompts(questions, { onSubmit });
-		console.log(response)
+
+		// Init.
+		clearConsole();
+
+		// Start.
+		console.log('\n'); // eslint-disable-line no-console
+		console.log(
+			'📦 ',
+			chalk.black.bgYellow(` Downloading WPGulp files in: → ${chalk.bgGreen(` ${theDir} `)}\n`),
+			chalk.dim(`\n In the directory: ${theCWD}\n`),
+			chalk.dim('This might take a couple of minutes.\n')
+		);
+
+		const spinner = ora({ text: '' });
+		spinner.start(`1. Creating WPGulp files inside → ${chalk.black.bgWhite(` ${theDir} `)}`);
+
+		if (response.value === "upgrade") {
+			execDownload(files.gulpFile, files.packageJSONFile)
+		}
+		else if (response.value === "install") {
+			execDownload(files.gulpFile, files.packageJSONFile, files.configFile)
+		}
 	})();
-	// Init.
-	/*clearConsole();
-
-	// Files.
-	const filesToDownload = [
-		'https://raw.githubusercontent.com/ricardotormo/WPGulp/master/src/gulpfile.babel.js',
-		'https://raw.githubusercontent.com/ricardotormo/WPGulp/master/src/package.json',
-		'https://raw.githubusercontent.com/ricardotormo/WPGulp/master/src/wpgulp.config.js'
-	];
-
-	// Start.
-	console.log('\n'); // eslint-disable-line no-console
-	console.log(
-		'📦 ',
-		chalk.black.bgYellow(` Downloading WPGulp files in: → ${chalk.bgGreen(` ${theDir} `)}\n`),
-		chalk.dim(`\n In the directory: ${theCWD}\n`),
-		chalk.dim('This might take a couple of minutes.\n')
-	);
-
-	const spinner = ora({ text: '' });
-	spinner.start(`1. Creating WPGulp files inside → ${chalk.black.bgWhite(` ${theDir} `)}`);
-
-	// Download.
-	Promise.all(filesToDownload.map(x => download(x, `${theCWD}`))).then(async () => {
-		spinner.succeed();
-
-		// The npm install.
-		spinner.start('2. Installing npm packages...');
-		// await execa('npm', ['install', '--silent']);
-		await execa('npm', ['install']);
-		spinner.succeed();
-
-		// Done.
-		printNextSteps();
-	});*/
 };
